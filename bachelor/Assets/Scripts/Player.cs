bd5d592856454dfe3bf3ff8b7b90b0447f52759a
chickenduy <khanhduydinhle@gmail.com>
2015-12-26 18:14:17 +0100

    Import
    Import:
    - Standard Assets (Water)
    - Materials (Ice, Grass, ...
    
    Created
    - Ice Block
    - Spawns for player

---
+++ bachelor/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    //Spawn Points
+    public Transform playerSpawnPoints;
+    public bool respawn = false;
+
+    private Transform[] spawnPoint;
+    private bool toggle = false;
+
+
+	// Use this for initialization
+	void Start () {
+        spawnPoint = playerSpawnPoints.GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(toggle != respawn)
+        {
+            Respawn();
+            respawn = false;
+        }
+        else
+        {
+            toggle = respawn;
+        }
+	
+	}
+
+    private void Respawn()
+    {
+        int i = Random.Range(0, spawnPoint.Length);
+        transform.position = spawnPoint[i].transform.position;
+    }
+}
