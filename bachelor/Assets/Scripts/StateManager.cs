f3d10204dc683cf0c8928288b7b161305c33a84f
chickenduy <khanhduydinhle@gmail.com>
2015-12-28 18:15:56 +0100

    Scripts and Blackfire Studio
    Added:
    - new torch model
    - small scripts
    - old realtime lighting model again
    - Blackfire Assets
    - some occlusion maps

---
+++ bachelor/Assets/Scripts/StateManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateManager : MonoBehaviour {
+
+    public GameObject firePlace;
+    public GameObject window;
+    public GameObject ceilingLight;
+    public GameObject ceilingFan;
+
+    public GameObject torches;
+    private Light[] torch;
+
+    private int temperatureIndex;
+    private int lightIndex;
+    private float peeIndex;
+    private bool windIndex;
+
+
+    // Use this for initialization
+    void Start() {
+        torch = torches.GetComponentsInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            torchesOut();
+        }
+    }
+
+    private void torchesOut()
+    {
+        for(int i = 0; i<torch.Length; i++)
+        {
+            torch[i].enabled = false;
+        }
+        print("unlit");
+    }
+}
