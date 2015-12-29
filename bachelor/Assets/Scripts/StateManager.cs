b7c4d89432b14415d58ba40cfcb85789d0db367c
chickenduy <khanhduydinhle@gmail.com>
2015-12-29 14:18:49 +0100

    Torches into Maze

--- bachelor/Assets/Scripts/StateManager.cs
+++ bachelor/Assets/Scripts/StateManager.cs
@@ -10,22 +10,35 @@ public class StateManager : MonoBehaviour {
 
     public GameObject torches;
     private Light[] torch;
+    private ParticleSystem[] torchflame;
 
     private int temperatureIndex;
     private int lightIndex;
     private float peeIndex;
     private bool windIndex;
 
-
     // Use this for initialization
     void Start() {
         torch = torches.GetComponentsInChildren<Light>();
+        torchflame = torches.GetComponentsInChildren<ParticleSystem>();
+        temperatureIndex = 0;
+        lightIndex = 0;
+        peeIndex = 0;
+        windIndex = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("k"))
+        {
+            temperatureIndex = temperatureIndex - 1;
+            print(temperatureIndex);
+        }
+
+
+        if(temperatureIndex == -1)
         {
             torchesOut();
         }
@@ -36,6 +49,12 @@ public class StateManager : MonoBehaviour {
         for(int i = 0; i<torch.Length; i++)
         {
             torch[i].enabled = false;
+        }
+        for (int i = 0; i < torch.Length; i++)
+        {
+            var em = torchflame[i].emission;
+            torchflame[i].Clear();
+            em.enabled = false;
         }
         print("unlit");
     }
