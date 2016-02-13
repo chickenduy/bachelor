3f39c5b4faead364fb6f5ea7e3f9a73578641c91
chickenduy <khanhduydinhle@gmail.com>
2016-02-13 15:59:37 +0100

    added UI for mirrors

--- bachelor/Assets/Scripts/OBJ/Maze/Mirror_OBJ.cs
+++ bachelor/Assets/Scripts/OBJ/Maze/Mirror_OBJ.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class Mirror_OBJ : MonoBehaviour
 {
-    private int id;
+    private int _id;
+    public int id
+    {
+        get
+        {
+            return _id;
+        }
+    }
 
     // Use this for initialization
     void Start()
     {
-        id = GetComponentInParent<Maze_Room_OBJ>().id;
+        _id = GetComponentInParent<Maze_Room_OBJ>().id;
         gameObject.GetComponent<Renderer>().material = Maze_S.Instance.mirror;
-        Maze_S.Instance.Register(gameObject.GetComponent<Renderer>(), id);
+        Maze_S.Instance.Register(gameObject.GetComponent<Renderer>(), _id);
 
     }
 
