1370392e3ce66ee6fd78fa22ea31264e93b26637
chickenduy <khanhduydinhle@gmail.com>
2016-02-14 19:29:01 +0100

    fixed arms

---
+++ bachelor/Assets/TEST.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEST : MonoBehaviour {
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.RecalculateBounds();
+    }
+}
