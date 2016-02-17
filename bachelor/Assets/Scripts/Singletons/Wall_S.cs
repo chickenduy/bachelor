67bda9805e3ab86d001be0e3abac12c947754c73
chickenduy <khanhduydinhle@gmail.com>
2016-02-17 03:59:06 +0100

    clean up Wall_S

--- bachelor/Assets/Scripts/Singletons/Wall_S.cs
+++ bachelor/Assets/Scripts/Singletons/Wall_S.cs
@@ -7,15 +7,19 @@ public class Wall_S : Singleton<Wall_S>
     // guarantee this will be always a singleton only - can't use the constructor!
     protected Wall_S() { }
 
-    public int wall_timer;
-    public int wall_Final_Timer;
 
-    //variables
+
+    //private
     private Dictionary<GameObject, int> wall_dictionary = new Dictionary<GameObject, int>();
     private Dictionary<int, Animator> wall_animator = new Dictionary<int, Animator>();
-    private GameObject wall_2;
     private List<Animator> wall_final_dictionary = new List<Animator>();
+    private GameObject wall_2;
 
+    //private visible
+    [SerializeField]
+    private int wall_timer;
+    [SerializeField]
+    private int wall_Final_Timer;
 
     //methods
     void Start()
